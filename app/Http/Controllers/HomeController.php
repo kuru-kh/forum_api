faec74e8f5ad246d61d6b4f4ca931443559c0e53
kuruprashath <kuru24019@gmail.com>
2022-09-06 21:23:43 +0530

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use App\Services\LuckySeven;
+use App\Services\SupplierService;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+}
